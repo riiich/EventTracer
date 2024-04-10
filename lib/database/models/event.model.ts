cf948ca7406ab135376a4467f22c2faa642c9b50
Rich <horich21@gmail.com>
2024-04-10 00:07:49 -0700

    Fixed createdAt type in Event model. Before it was createdAt: User -> createdAt: Date

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -2,7 +2,7 @@ import { Document, Schema, model, models } from "mongoose";
 import User from "./user.model";
 
 export interface IEvent extends Document {
-    _id: string;
+	_id: string;
 	title: string;
 	description?: string;
 	location?: string;
@@ -10,13 +10,12 @@ export interface IEvent extends Document {
 	url?: string;
 	isFree: Boolean;
 	createdAt: Date;
-	dateCreated: Date;
 	startDateTime: Date;
 	endDateTime: Date;
 	imageUrl: string;
 	attendanceCount: number;
-	category: {_id: string, name: string};
-	organizer: {_id: string, firstName: string, lastName: string};
+	category: { _id: string; name: string };
+	organizer: { _id: string; firstName: string; lastName: string };
 }
 
 const EventSchema = new Schema({
@@ -26,10 +25,9 @@ const EventSchema = new Schema({
 	price: { type: String },
 	url: { type: String },
 	isFree: { type: Boolean, default: false },
-	createdAt: { type: User, required: true },
-	dateCreated: { type: Date, default: Date.now },
-	startDateTime: { type: Date, required: true },
-	endDateTime: { type: Date, required: true },
+	createdAt: { type: Date, default: Date.now },
+	startDateTime: { type: Date, default: Date.now },
+	endDateTime: { type: Date, default: Date.now },
 	imageUrl: { type: String, required: true },
 	attendanceCount: { type: Number, default: 0 },
 	category: { type: Schema.Types.ObjectId, ref: "Category" },
